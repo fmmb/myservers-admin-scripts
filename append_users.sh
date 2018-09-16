273cfb64fb509607da61a3afeeb69ad8bc5ebfb5
fmmb <fernando.batista.pt@gmail.com>
2018-09-16 10:22:26 +0100

    ajustes inspirados nas alteracoes de TM

--- append_users.sh
+++ append_users.sh
@@ -25,11 +25,11 @@ fi
 
 echo "Ok, Ready to append additional students"
 
-cat $newusers | iconv -f UTF-8 -t 'ASCII//TRANSLIT' | while read line; do
+cat $newusers | iconv -f UTF-8 -t 'ASCII//TRANSLIT' | tr -d "'" | while read line; do
   username=a$( echo "$line" | awk -F'\t' '{print $1}')
   match=$(grep -c "^$username" /etc/passwd )
   if [ $match -ne 0 ]; then
-     echo "Skipping: user $username already exists"
+     echo "skipping $username, user already exists"
   else
     nome=$( echo "$line" | awk -F'\t' '{print $2}')
     email=$( echo "$line" | awk -F'\t' '{print $3}')
--- reset_password.sh
+++ reset_password.sh
@@ -11,17 +11,18 @@ username=$2
 match=$(grep -c "^$username:" /etc/passwd )
 if [ $match -ne 0 ]; then
     name=$( cat /etc/passwd| grep "^$username:" | awk -F '[:,]' '{print $5}')
-    grp=$(cat /etc/group | grep "$(cat /etc/passwd | grep "^$username" | cut -d':' -f4)" | cut -d':' -f1 | head -1)
-    if [ -f mensagem.$grp.txt ]; then
-       echo "Sending the content of mensagem.$grp.txt"
+    group=$(cat /etc/group | grep "$(cat /etc/passwd | grep "^$username" | cut -d':' -f4)" | cut -d':' -f1 | head -1)
+    if [ -f mensagem.$group.txt ]; then
+       echo "Sending the content of mensagem.$group.txt"
     else
-      echo "error: File not found: mensagem.$grp.txt"
+      echo "error: File not found: mensagem.$group.txt"
       exit 1
     fi
     password=$(./create_random_passwd.py)
 	#echo "($password)"
     echo $password | awk '{print $0; print $0}' | passwd $username
-    cat mensagem.$grp.txt | sed "s/#NAME#/$name/g;s/#USERNAME#/$username/g;s/#PASSWORD#/$password/g" | ./envia_mail.py "$email"
+    cat mensagem.$group.txt | sed "s/#NAME#/$name/g;s/#USERNAME#/$username/g;s/#PASSWORD#/$password/g" | ./envia_mail.py "$email"
+    #echo "cat mensagem.$group.txt | sed \"s/#NAME#/$name/g;s/#USERNAME#/$username/g;s/#PASSWORD#/$password/g\" | ./envia_mail.py \"$email\"" >> comandos-a-executar-no-servidor-smtp.sh
 else
    echo "Skipping: user $username does not exists"
 fi
